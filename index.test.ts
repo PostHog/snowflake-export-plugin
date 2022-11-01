40e7dec9ead62c72302f0359c126478ba6d9a9c5
Harry Waye <harry@microwayes.net>
2022-11-01 06:55:53 +0000

    chore: update jest, use swc
    For some reason I was having issues with jest 27 and everything timing
    out so I:
    
     1. updated jest to 29
     2. updated tests to use modern timers jest option
     3. I also had issues with the jest test runner, so I also switched to
        swc

--- index.test.ts
+++ index.test.ts
@@ -7,7 +7,7 @@ import { setupServer } from "msw/node"
 import { rest } from "msw"
 import zlib from "zlib"
 
-jest.setTimeout(50000)
+jest.setTimeout(5000)
 
 test("handles events", async () => {
     // Checks for the happy path
@@ -36,7 +36,8 @@ test("handles events", async () => {
     const s3 = new S3()
     await s3.createBucket({ Bucket: bucketName }).promise()
 
-    const meta = {
+    let meta = {}
+    Object.assign(meta, {
         attachments: {}, config: {
             account: snowflakeAccount,
             username: "username",
@@ -60,17 +61,13 @@ test("handles events", async () => {
             forceCopy: 'Yes' as const,
             debug: 'ON' as const,
         },
-        jobs: { retryCopyIntoSnowflake: async (payload) => { 
-            await snowflakePlugin.copyIntoTableFromStage(payload) 
-            return jest.fn()
-        } 
-    },
+        jobs: createJobs(snowflakePlugin.jobs)(meta),
         cache: cache,
         storage: storage,
         // Cast to any, as otherwise we don't match plugin call signatures
         global: {} as any,
         geoip: {} as any
-    }
+    })
 
     const events = [
         {
@@ -125,7 +122,8 @@ test("handles > 1k files", async () => {
     const snowflakeAccount = uuid4()
 
     const snowflakeMock = jest.fn()
-    const meta = {
+    let meta = {}
+    Object.assign(meta, {
         attachments: {}, config: {
             account: snowflakeAccount,
             username: "username",
@@ -149,32 +147,42 @@ test("handles > 1k files", async () => {
             forceCopy: 'Yes' as const,
             debug: 'ON' as const,
         },
-        jobs: { retryCopyIntoSnowflake: () => ({ runIn: snowflakeMock } )},
+        jobs: createJobs(snowflakePlugin.jobs)(meta),
         cache: cache,
         storage: storage,
         // Cast to any, as otherwise we don't match plugin call signatures
-        global: {snowflake: {copyIntoTableFromStage: snowflakeMock}} as any,
+        global: { snowflake: { copyIntoTableFromStage: snowflakeMock } } as any,
         geoip: {} as any
-    }
+    })
 
     await storage.set('_files_staged_for_copy_into_snowflake', Array(2100).fill('file'))
     await cache.expire('lastRun', 0)
-    await snowflakePlugin.runEveryMinute(meta) 
+    await snowflakePlugin.runEveryMinute(meta)
 
-   expect(snowflakeMock.mock.calls.length).toBe(42)
+    expect(snowflakeMock.mock.calls.length).toBe(42)
 
 })
+
+const createJob = (job: (payload, meta) => null) => (meta: any) => (payload: any) => ({
+    runIn: async (runIn: number, unit: string) => {
+        await new Promise((resolve) => setTimeout(resolve, runIn))
+        await job(payload, meta)
+    }
+})
+
+const createJobs = (jobs: any) => (meta: any) => Object.fromEntries(Object.entries(jobs).map(([jobName, jobFn]) => [jobName, createJob(jobFn)(meta)]))
 
 
 // Use fake timers so we can better control e.g. backoff/retry code.
 // Use legacy fake timers. With modern timers there seems to be little feedback
 // on fails due to test timeouts.
 beforeEach(() => {
-    jest.useFakeTimers("legacy")
+    jest.useFakeTimers({ advanceTimers: 30 })
 })
 
 afterEach(() => {
     jest.runOnlyPendingTimers()
+    jest.clearAllTimers()
     jest.useRealTimers()
 })
 
@@ -267,7 +275,7 @@ const createSnowflakeMock = (accountName: string) => {
         //
         //  1. POST your SQL query up to the API, the resulting resource
         //     identified via a query id. However, we don't actually need to do
-        //     anything explicitly with this id, rather we use we...
+        //     anything explicitly with this id, rather we...
         //  2. use the getResultUrl to fetch the results of the query
         //
         // TODO: handle case when query isn't complete on requesting getResultUrl

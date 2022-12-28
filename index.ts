941d009c69fb9e0357b7cd6c119165069df1812d
James Greenhill <fuziontech@gmail.com>
2022-12-27 19:11:35 -0800

    add retry logic for the connection promise

--- index.ts
+++ index.ts
@@ -46,6 +46,32 @@ interface SnowflakePluginInput {
     storage: StorageExtension
 }
 
+/**
+ * Util function to return a promise which is resolved in provided milliseconds
+ */
+function waitFor(millSeconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, millSeconds);
+  });
+}
+
+async function retryPromiseWithDelay(promise, nthTry, delayTime) {
+  try {
+    const res = await promise;
+    return res;
+  } catch (e) {
+    if (nthTry === 1) {
+      return Promise.reject(e);
+    }
+    console.log('retrying', nthTry, 'time');
+    // wait for delayTime amount of time before calling this method again
+    await waitFor(delayTime);
+    return retryPromiseWithDelay(promise, nthTry - 1, delayTime);
+  }
+}
+
 interface TableRow {
     uuid: string
     event: string
@@ -329,7 +355,7 @@ class Snowflake {
                         ...roleConfig,
                     })
 
-                    await new Promise<string>((resolve, reject) =>
+                    await retryPromiseWithDelay(new Promise<string>((resolve, reject) =>
                         connection.connect((err, conn) => {
                             if (err) {
                                 console.error('Error connecting to Snowflake: ' + err.message)
@@ -338,7 +364,7 @@ class Snowflake {
                                 resolve(conn.getId())
                             }
                         })
-                    )
+                    ), 5, 5)
 
                     return connection
                 },
